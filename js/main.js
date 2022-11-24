a4581b4340c4fff2c6a91fb31f6790f981d9f72c
j-hyerim <112531998+j-hyerim@users.noreply.github.com>
2022-11-24 18:24:35 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', function () {
   console.log(window.scrollY); //스크롤값 확인하기
 });
 
 // // content가 화면에 80% 지점일 때 동작하는 스크립트
 const spyEl = document.querySelectorAll('section.scroll-spy');
 
-spyEl.forEach(function(spyEl){
+spyEl.forEach(function (spyEl) {
   new ScrollMagic.Scene({
-    triggerElement:spyEl,
-    triggerHook:0.8,
+    triggerElement: spyEl,
+    triggerHook: 0.8,
   })
-  .setClassToggle(spyEl,'show')
-  .addTo(new ScrollMagic.Controller());
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller());
 });
 
 //protfolio 버튼 클릭하면 아래 박스 열리게
@@ -21,49 +21,49 @@ const promotionEl2 = document.querySelector('div.about');
 const toggleBtnEl = document.querySelector('#port_btn');
 const aboutBtnEl2 = document.querySelector('#about_btn');
 
-toggleBtnEl.addEventListener('click',function(){
-  if(promotionEl.classList.contains('hide')){
+toggleBtnEl.addEventListener('click', function () {
+  if (promotionEl.classList.contains('hide')) {
     promotionEl.classList.remove('hide');
-  }else{
+  } else {
     promotionEl.classList.add('hide');
   }
 });
-aboutBtnEl2.addEventListener('click',function(){
-  if(promotionEl2.classList.contains('hide')){
+aboutBtnEl2.addEventListener('click', function () {
+  if (promotionEl2.classList.contains('hide')) {
     promotionEl2.classList.remove('hide');
-  }else{
+  } else {
     promotionEl2.classList.add('hide');
   }
 });
 
-$("html, body").on('mousewheel DOMMouseScroll',function(e){
+$("html, body").on('mousewheel DOMMouseScroll', function (e) {
 
   var wheelDelta = e.originalEvent.wheelDelta;
-      console.log(wheelDelta);
-      
-  if(wheelDelta > 0){
+  console.log(wheelDelta);
+
+  if (wheelDelta > 0) {
     console.log("up");
     $(this).scrollLeft(-wheelDelta + $(this).scrollLeft());
-  }else{
-  console.log("down");
+  } else {
+    console.log("down");
     $(this).scrollLeft(-wheelDelta + $(this).scrollLeft());
   }
-    });
+});
 
-$(document).ready(function(){
+$(document).ready(function () {
 
   //portfolio 버튼 클릭하면 animate 효과가 멈춤
-  $("#port_btn").click(function(){
+  $("#port_btn").click(function () {
     $(this).removeClass("animate");
-    $(".mouseIcon").css({"opacity":"1"});
+    $(".mouseIcon").css({ "opacity": "1" });
   });
 
   //top 버튼을 누르면 contents가 닫힘
-  $(".top_btn").click(function(){
-  $(".portfolio").addClass("hide");
+  $(".top_btn").click(function () {
+    $(".portfolio").addClass("hide");
   });
-  $(".top_btn2").click(function(){
-  $(".about").addClass("hide");
+  $(".top_btn2").click(function () {
+    $(".about").addClass("hide");
   });
 
 });
